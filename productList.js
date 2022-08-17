10b004071715a899909836eb3405927a216f8b48
Deep <deepgojariya123@gmail.com>
2022-08-18 00:57:25 +0530

    Added Delete functionality

--- productList.js
+++ productList.js
@@ -5,21 +5,27 @@ function loadProducts(){
         var data=JSON.parse(localStorage.getItem(localStorage.key(i)))
         var table = document.getElementById("products_list")
         var row = document.createElement("tr")
+        
         row.innerHTML = `
                         <td>${i+1}</td>
                         <td>${data.name}</td>
                         <td>${data.unit}</td>
                         <td>${data.desc}</td>
                         <td>${data.price}</td>
-                        <td><button class="btn-edit" onclick="location.href='index.html' ">Edit</button></td>
-                        <td><button class="btn-delete">Delete</button></td>
-                        
+                        <td><button class="btn-edit" onclick="location.href='index.html'">Edit</button></td>
+                        <td><button id="${data.name}" class="btn-delete">Delete</button></td>
                         `
         table.appendChild(row)
+
+        var deleteBtn = document.getElementById(`${data.name}`)
+        
+        deleteBtn.addEventListener("click",()=>{
+            localStorage.removeItem(data.name)        
+        })
     }
 }
 
-function deleteProduct(name){
-    localStorage.removeItem(name)
-}
+
+
+
 
